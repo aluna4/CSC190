2761ba5c45761b32146dc6e59eabfcb8478548c5
aluna4 <lunaalicia65@gmail.com>
2024-02-11 19:08:19 -0800

    changed destination from addRule to user

--- PhoenixFirewall/main/views.py
+++ PhoenixFirewall/main/views.py
@@ -19,7 +19,7 @@ def login_view(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            return render(request, 'AddRule.html')
+            return render(request, 'User.html')
         else:
             messages.error(request, 'Invalid username or password')
     return render(request, 'login.html')
