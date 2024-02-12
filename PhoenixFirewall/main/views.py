b38af3407bdb6e6deee33a3ca15bd00dd669ab1f
aluna4 <lunaalicia65@gmail.com>
2024-02-11 18:58:55 -0800

    added home button to login page and checked login credentials - displays message of incorrect username/password

--- PhoenixFirewall/main/views.py
+++ PhoenixFirewall/main/views.py
@@ -19,9 +19,9 @@ def login_view(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            return render(request, 'User.html')
+            return render(request, 'AddRule.html')
         else:
-            messages.error(request, 'Invalid username or password.')
+            messages.error(request, 'Invalid username or password')
     return render(request, 'login.html')
 
 #user page
